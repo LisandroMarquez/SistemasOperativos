5985b8322d5c48f4fd39d07cee44adeca9457c45
Lisandro Marquez <lichimarquez04@gmail.com>
2025-08-13 09:08:36 -0300

    lab0 kickstart

---
+++ lab0/entregable.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# EJERCICIO 1: Modelo del procesador
+[su comando aquí]
+
+# EJERCICIO 2: Número de unidades de ejecución (cores)
+[su comando aquí]
+
+# EJERCICIO 3: Lista de nombres de usuarios para red social de superhéroes
+[su comando aquí]
+
+# EJERCICIO 4A: Día de máxima temperatura en Córdoba
+[su comando aquí]
+
+# EJERCICIO 4B: Día de mínima temperatura en Córdoba
+[su comando aquí]
+
+# EJERCICIO 5: Ordenar jugadores de tenis por ranking
+[su comando aquí]
+
+# EJERCICIO 6: Ordenar tabla de Superliga por puntos y diferencia de goles
+[su comando aquí]
+
+# EJERCICIO 7: MAC address de la placa WiFi del equipo
+[su comando aquí]
+
+# EJERCICIO 8: Renombrar archivos de subtítulos
+[su comando aquí]
+
+# EJERCICIO 9A: Recortar video con ffmpeg
+[su comando aquí]
+
+# EJERCICIO 9B: Mezclar audio con ffmpeg
+[su comando aquí]
