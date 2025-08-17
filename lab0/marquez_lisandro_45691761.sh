a932f9882473f80b7b34e9d869c7edd2441b6953
Lisandro Marquez <lichimarquez04@gmail.com>
2025-08-17 12:59:16 -0300

    lab0 finalizado

--- lab0/marquez_lisandro_45691761.sh
+++ lab0/marquez_lisandro_45691761.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
-
 # EJERCICIO 1: Modelo del procesador
-[su comando aquí]
-
+grep -i -m 1 'model name' /proc/cpuinfo | cut -d: -f2- | cut -d' ' -f2-
 # EJERCICIO 2: Número de unidades de ejecución (cores)
-[su comando aquí]
-
+grep -i 'model name' /proc/cpuinfo | wc -l
 # EJERCICIO 3: Lista de nombres de usuarios para red social de superhéroes
-[su comando aquí]
-
+cut -d';' -f2 ./datos/heroes.csv | grep -v '^$' | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g' | tail -n +2 > superheroes_usuarios.txt
 # EJERCICIO 4A: Día de máxima temperatura en Córdoba
-[su comando aquí]
-
+cut -d' ' -f1-3,5 ./datos/weather_cordoba.in | sort -k4 -nr | head -n1 | cut -d' ' -f1-3 | sed 's/ /-/g'
 # EJERCICIO 4B: Día de mínima temperatura en Córdoba
-[su comando aquí]
-
+cut -d' ' -f1-3,6 ./datos/weather_cordoba.in | sort -k4 -n | head -n1 | cut -d' ' -f1-3 | sed 's/ /-/g'
 # EJERCICIO 5: Ordenar jugadores de tenis por ranking
-[su comando aquí]
-
+sort -k3 -n ./datos/atpplayers.in
 # EJERCICIO 6: Ordenar tabla de Superliga por puntos y diferencia de goles
-[su comando aquí]
-
+awk '{diff = $7 - $8; print $0, diff}' ./datos/superliga.in | sort -k2 -k9 -nr | cut -d' ' -f1-8
 # EJERCICIO 7: MAC address de la placa WiFi del equipo
-[su comando aquí]
-
+ip -br link show up | grep -E '^(en|eth)' | grep -m1 -oE '([[:xdigit:]]{2}:){5}[[:xdigit:]]{2}'
 # EJERCICIO 8: Renombrar archivos de subtítulos
-[su comando aquí]
-
+for f in serie_prueba/*_es.srt; do mv "$f" "${f%_es.srt}.srt"; done
 # EJERCICIO 9A: Recortar video con ffmpeg
-[su comando aquí]
-
+ffmpeg -ss 5 -to 55 -i ./ffmpeg_files/test.mp4 -c copy ./ffmpeg_files/edited.mp4
 # EJERCICIO 9B: Mezclar audio con ffmpeg
-[su comando aquí]
+ffmpeg -i ./ffmpeg_files/ala_delta.mp3 -i ./ffmpeg_files/burn.mp3 -filter_complex "amix=inputs=2:duration=shortest:dropout_transition=2" ./ffmpeg_files/remix.mp3
